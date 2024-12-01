8488763654d511770741b5d8d3a9b3ff481b35e6
Jeyhun Naghiyev <167631924+Ceyhun2056@users.noreply.github.com>
2024-12-01 18:37:29 +0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -65,3 +65,8 @@ document.addEventListener('DOMContentLoaded', function() {
 document.getElementById('dark-mode-toggle').addEventListener('click', function() {
         document.body.classList.toggle('dark-mode');
     });
+
+
+
+
+    
